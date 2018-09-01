0e5b9c5325b907b58483e4527e094edecace323e
Eiji Uema <eijiuema@gmail.com>
2018-09-01 18:54:00 -0300

    Player now shoots nicely

--- Shooter.gd
+++ Shooter.gd
@@ -5,7 +5,7 @@ func shoot(Bullet, direction, speed = null, size = null, damage = null):
 	var shooter_radius = get_node("CollisionShape2D").get_shape().radius
 	var bullet_radius = bullet.get_node("CollisionShape2D").get_shape().radius
 	var total_radius = shooter_radius + bullet_radius
-	bullet.position = position + Vector2(total_radius, total_radius) * 2 * direction
+	bullet.position = position + Vector2(total_radius, total_radius) * 4 * direction
 	bullet.init(direction, speed, size, damage)
 	get_tree().get_root().add_child(bullet)
 	return bullet
