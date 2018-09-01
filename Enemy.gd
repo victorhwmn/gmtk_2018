13ea939dfeb7707bbedbe627275b2010122b2f3e
Victor Watanabe <victorhwmn@gmail.com>
2018-09-01 10:25:35 -0300

    Enemy can shot

--- Enemy.gd
+++ Enemy.gd
@@ -5,6 +5,7 @@ extends KinematicBody2D
 var shot_timer = 3;
 var timer = null;
 var can_shot = false;
+var Bullet = preload("res://Bullet.tscn")
 onready var acha_player = get_node("Detectar_jogador");
 onready var player = get_node("../Player")
 
@@ -15,8 +16,9 @@ func _ready():
 	timer = Timer.new()
 	timer.set_wait_time(shot_timer);
 	timer.connect("timeout",self,"_can_shot")
+	timer.set_one_shot(true);
 	add_child(timer);
-	timer.start();
+	timer.start()
 
 	pass
 
@@ -28,6 +30,8 @@ func _process(delta):
 		var direcao = player.position
 		if can_shot == true :
 			can_shot = false;
+			_shot(direcao)
+			timer.start();
 			print(direcao);
 			#Cria uma bala na direção do Player
 	
@@ -35,10 +39,21 @@ func _process(delta):
 #	# Update game logic here.
 
 	pass
+func _shot(direcao) :
+	var shoot_dir = -(position - direcao).normalized()
+		
+	var bullet = Bullet.instance()
+	bullet.position = position + get_node("Sprite").texture.get_size()/2 * shoot_dir
+	bullet.init(shoot_dir)
+	get_parent().add_child(bullet)
+
+
 
 func _can_shot():
 	can_shot = true;
 
+func hit():
+	queue_free()
 	
 	
 	
