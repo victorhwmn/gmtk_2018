6a45968e369bd8db26f1038fc10b83213bbd3b2a
Victor Watanabe <victorhwmn@gmail.com>
2018-09-01 21:41:00 -0300

    Esteira corrigida / Criação de uma fase

--- Esteira.gd
+++ Esteira.gd
@@ -4,11 +4,20 @@ extends Area2D
 # var a = 2
 # var b = "textvar"
 var Arraybodies;
-export (String, "Up", "Left", "Right","Down") var type = "Up";
+export (String, "Up", "Left", "Right","Down","LR") var type = "Up";
 var moviment = Vector2(0,0);
+var flag = 2;
 
 
 func _ready():
+	
+	if type == "LR":	
+		var timer = Timer.new();
+		timer.set_wait_time(5)	
+		timer.connect("timeout", self, "_timeout")
+		add_child(timer)
+		timer.start()
+
 
 	pass
 
@@ -19,24 +28,25 @@ func _process(delta):
 	Arraybodies = get_overlapping_bodies();
 	match type :
 		"Up":
-			moviment = Vector2(0,-1);
-			
+			moviment = Vector2(0,-2);
 		"Left":
-			moviment = Vector2(-1,0);
-			
+			moviment = Vector2(-2,0);
 		"Right":
-			moviment = Vector2(1,0);
-			
+			moviment = Vector2(2,0);
 		"Down":
-			moviment = Vector2(0,1);
-	
+			moviment = Vector2(0,2);
+		"LR" : 
+			moviment = Vector2(flag,0);
+			
+			
 	
 	for i in Arraybodies.size() :
 		body = Arraybodies[i];
-		if body.is_in_group("enemy") :
+		if body.is_in_group("damageable") :
 			body.move_and_collide(moviment)
 			
-		
-			
+func _timeout() :
+	flag = flag * -1;
+	rotate(PI);
 	
 
